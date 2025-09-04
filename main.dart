60eb838d9fbcc53a18e0aa66fcf59e4d95f937f2
leehalo <123772141+leehalo@users.noreply.github.com>
2025-09-04 00:05:20 +0000

    feat: if statement

--- main.dart
+++ main.dart
@@ -1,1 +1,12 @@
-print("Olá mundo!");
+import 'dart:io';
+void main(){
+    int idade = 20;
+    if (idade <=18)
+ {
+    print('Entrada proibida');
+    return;
+ }
+ print ('Entrada  liberada');
+    
+}
+
